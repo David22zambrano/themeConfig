da8155d4415be097c87aad49a3e31ead0122ce20
David22zambrano <david.zambrano17@outlook.com>
2022-12-14 09:32:20 -0500

    theming -- Not finished yet

--- theming/src/app/app.module.ts
+++ theming/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MaterialModule } from './material.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MAT_SNACK_BAR_DATA} from '@angular/material/snack-bar';
+
 
 @NgModule({
   declarations: [
@@ -12,9 +17,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [{ provide: MAT_SNACK_BAR_DATA, useValue: {} }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
