a39e06ec4b09ee8d0e37ce012e77819d467244f8
Sebastian0916 <Sebas.vera0916@gmail.com>
2022-12-19 18:07:49 -0500

    Revert "Revert "ajuste de app module""
    This reverts commit 29a7aaa2ee515fc22725e9490591c799bd706881.

--- theming/src/app/components/checkbox/checkbox.componet.stories.ts
+++ theming/src/app/components/checkbox/checkbox.componet.stories.ts
@@ -1,7 +1,19 @@
-import { storiesOf, moduleMetadata } from "@storybook/angular";
-import { CheckboxComponent, Checkbox } from "./checkbox.component";
-
-const checkbox:Checkbox = {
-title:'Hola',
-done: false
-}
+import { storiesOf, moduleMetadata } from '@storybook/angular';
+import { CheckboxComponent, Checkbox } from './checkbox.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+const checkbox: Checkbox = {
+  title: 'Hola',
+  done: false,
+};
+storiesOf('checkbox', module)
+  .addDecorator(
+    moduleMetadata({
+      declarations: [CheckboxComponent],
+    })
+  )
+  .add('Checkbox small ', () => {
+    return {
+      template: '<app-checkbox></app-checkbox>',
+      props: checkbox,
+    };
+  });
